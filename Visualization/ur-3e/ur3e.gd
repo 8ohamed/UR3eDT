4e6ba2c140477cbd4d177da55ebd94a2a0bebcef
Thomas Reifling <thomas@reifling.net>
2026-04-23 10:18:51 +0200

    Updated godot

--- Visualization/ur-3e/ur3e.gd
+++ Visualization/ur-3e/ur3e.gd
@@ -12,21 +12,20 @@ func _on_message(message):
 	var q_actual = data.q_actual
 	if q_actual:
 		print(q_actual)
-		var joint_0_val = %UR3e/b/mk_0 as Node3D
-		joint_0_val.rotation.z =  - q_actual[0]
-		#joint_0_val.transform3D()
+		var j0 = %UR3e/b/mk_0 as Marker3D
+		j0.rotation.z =  - q_actual[0]
 		
-		var joint_1_val = %UR3e/b/mk_0/j0/mk_1 as Marker3D
-		joint_1_val.rotation.y = - q_actual[1] - PI / 2
+		var j1 = %UR3e/b/mk_0/j0/mk_1 as Marker3D
+		j1.rotation.y = - q_actual[1] - PI / 2
 		
-		var joint_2_val = %UR3e/b/mk_0/j0/mk_1/j1/mk_2 as Marker3D
-		joint_2_val.rotation.y = -q_actual[2]
+		var j2 = %UR3e/b/mk_0/j0/mk_1/j1/mk_2 as Marker3D
+		j2.rotation.y = -q_actual[2]
 
-		var joint_3_val = %UR3e/b/mk_0/j0/mk_1/j1/mk_2/j2/mk_3 as Marker3D
-		joint_3_val.rotation.y = - q_actual[3] - PI / 2
+		var j3 = %UR3e/b/mk_0/j0/mk_1/j1/mk_2/j2/mk_3 as Marker3D
+		j3.rotation.y = - q_actual[3] - PI / 2
 
-		var joint_4_val = %UR3e/b/mk_0/j0/mk_1/j1/mk_2/j2/mk_3/j3/mk_4 as Marker3D
-		joint_4_val.rotation.z = - q_actual[4]
+		var j4 = %UR3e/b/mk_0/j0/mk_1/j1/mk_2/j2/mk_3/j3/mk_4 as Marker3D
+		j4.rotation.z = - q_actual[4]
 
-		var joint_5_val = %UR3e/b/mk_0/j0/mk_1/j1/mk_2/j2/mk_3/j3/mk_4/j4/mk_5 as Marker3D
-		joint_5_val.rotation.y = - q_actual[5]
+		var j5 = %UR3e/b/mk_0/j0/mk_1/j1/mk_2/j2/mk_3/j3/mk_4/j4/mk_5 as Marker3D
+		j5.rotation.y = - q_actual[5]
